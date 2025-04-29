e6377c4d45f05a98eba79e14149f79d0e871379d
Zennoon <98653096+Zennoon@users.noreply.github.com>
2025-04-29 02:08:53 -0700

    Add docker file for easier deployment

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello World!" });
 })
 
-app.listen(3000, '0.0.0.0', () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
     console.log("Server is running on port 3000");
 })
