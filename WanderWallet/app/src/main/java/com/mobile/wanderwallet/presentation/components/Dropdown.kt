c0c10b87f0d48f2ee0a2bccea46aa5a44c4993cd
Saba Habtamu <sabahabtamuu@gmail.com>
2025-05-09 00:40:07 +0300

    Update Dropdown.kt

--- WanderWallet/app/src/main/java/com/mobile/wanderwallet/presentation/components/Dropdown.kt
+++ WanderWallet/app/src/main/java/com/mobile/wanderwallet/presentation/components/Dropdown.kt
@@ -1,4 +1,4 @@
-package com.app.pro.component
+package com.mobile.wanderwallet.presentation.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
